6d16a7d5683aa20a138198a58808b9fe68715233
Deven Wadhwa <wadhwadeven@gmail.com>
2022-12-18 19:52:56 +0530

    build: simple scanner

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,55 @@
-import { NativeModules, Platform } from 'react-native';
+import React from 'react';
+import type { ViewStyle } from 'react-native';
+import { Camera, useCameraDevices } from 'react-native-vision-camera';
+import {
+  Barcode,
+  BarcodeFormat,
+  useScanBarcodes,
+} from 'vision-camera-code-scanner';
 
-const LINKING_ERROR =
-  `The package 'ts-react-native-barcode-scanner' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
+const BarcodeScanner = ({
+  style,
+  callback,
+  camera,
+}: {
+  style: ViewStyle;
+  callback: (barcodes: Barcode[]) => void;
+  camera: 'back' | 'front';
+}) => {
+  const [hasPermission, setHasPermission] = React.useState(false);
+  const devices = useCameraDevices();
+  const device = camera === 'back' ? devices.back : devices.front;
 
-const TsReactNativeBarcodeScanner = NativeModules.TsReactNativeBarcodeScanner
-  ? NativeModules.TsReactNativeBarcodeScanner
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
+  const [frameProcessor, barcodes] = useScanBarcodes([BarcodeFormat.QR_CODE], {
+    checkInverted: true,
+  });
 
-export function multiply(a: number, b: number): Promise<number> {
-  return TsReactNativeBarcodeScanner.multiply(a, b);
-}
+  React.useEffect(() => {
+    (async () => {
+      const status = await Camera.requestCameraPermission();
+      setHasPermission(status === 'authorized');
+    })();
+  }, []);
+
+  React.useEffect(() => {
+    callback(barcodes);
+  }, [barcodes, callback]);
+
+  return device && hasPermission ? (
+    <>
+      <Camera
+        style={style}
+        device={device}
+        isActive={true}
+        frameProcessor={frameProcessor}
+        frameProcessorFps={5}
+      />
+    </>
+  ) : (
+    <></>
+  );
+};
+
+export type BarcodeType = Barcode;
+
+export default BarcodeScanner;
